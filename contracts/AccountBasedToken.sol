671ddf4c417ab3d918891c91408d02abd535b5e7
Marrmee <mhuberts2008@hotmail.com>
2022-08-07 12:08:06 +0200

    initial commit

---
+++ contracts/AccountBasedToken.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+/**
+@title Account-based token fir the BlueChipAlliance
+@author Marco Huberts
+@dev    Implementation of an Account-Based Token using ERC721.
+*/
